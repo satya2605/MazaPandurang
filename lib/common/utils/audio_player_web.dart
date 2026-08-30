eeaa9dc30b0a48bcf4149d6db36ab540f8b6c8d3
Sanketsingh Thakur <sanketthakur2345@gmail.com>
2026-08-30 08:53:58 +0530

    style: fix doc comments and lint warnings in audio player helper

--- lib/common/utils/audio_player_web.dart
+++ lib/common/utils/audio_player_web.dart
@@ -1,4 +1,4 @@
-// ignore: avoid_web_libraries_in_flutter
+// ignore: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:html' as html;
 
 void playWebAudio(String base64Audio, {String mimeType = 'audio/wav'}) {
